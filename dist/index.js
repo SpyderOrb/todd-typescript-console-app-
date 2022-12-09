b644dd4f9f84ab8aba0f71ae0e397087e7d4696e
SpyderOrb <alex.plachinda@gmail.com>
2022-12-10 00:31:42 +0100

    added inquirer

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 let todos = [
     new todoItem_1.TodoItem(1, "Buy Flowers"),
     new todoItem_1.TodoItem(2, "Get Shoes"),
@@ -16,9 +17,35 @@ let todoItem = collection.getTodoById(newId);
 // console.log(JSON.stringify(todoItem));
 // todoItem.printDetails();
 // collection.addTodo(todoItem);
-collection.getTodoItems(true).forEach((item) => item.printDetails());
 console.log(`${collection.userName}'s Todo List ` +
     `(${collection.getItemCounts().incomplete} items to do)` +
-    `\n (${collection.getItemCounts().total} all items)`);
+    `\n (${collection.getItemCounts().total} items in list)`);
+collection.getTodoItems(true).forEach((item) => item.printDetails());
 collection.removeComplete();
 collection.getTodoItems(true).forEach((item) => item.printDetails());
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List ` +
+        `(${collection.getItemCounts().incomplete}items to do)`);
+    collection.getTodoItems(true).forEach((item) => item.printDetails());
+}
+var Commans;
+(function (Commans) {
+    Commans["Quit"] = "Quit";
+})(Commans || (Commans = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer
+        .prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commans),
+    })
+        .then((answers) => {
+        if (answers["command"] !== Commans.Quit) {
+            promptUser();
+        }
+    });
+}
+promptUser();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
 
 let todos: TodoItem[] = [
   new TodoItem(1, "Buy Flowers"),
@@ -26,3 +27,34 @@ collection.getTodoItems(true).forEach((item) => item.printDetails());
 
 collection.removeComplete();
 collection.getTodoItems(true).forEach((item) => item.printDetails());
+
+function displayTodoList(): void {
+  console.log(
+    `${collection.userName}'s Todo List ` +
+      `(${collection.getItemCounts().incomplete}items to do)`
+  );
+  collection.getTodoItems(true).forEach((item) => item.printDetails());
+}
+
+enum Commans {
+  Quit = "Quit",
+}
+
+function promptUser(): void {
+  console.clear();
+  displayTodoList();
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose option",
+      choices: Object.values(Commans),
+    })
+    .then((answers) => {
+      if (answers["command"] !== Commans.Quit) {
+        promptUser();
+      }
+    });
+}
+
+promptUser();
